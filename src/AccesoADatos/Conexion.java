cfd72340016dc4e972152a4d78e1ac3c6efcd53f
Ismael7058 <estiloisma70@gmail.com>
2024-09-30 17:53:49 -0300

    Modificación en el package de Vistas
    Se genero un posible formato del JFrame y los JFrameInternal

--- src/AccesoADatos/Conexion.java
+++ src/AccesoADatos/Conexion.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AccesoADatos;
 
-/**
- *
- * @author juanz
- */
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Conexion {
-    
+
+    private static final String URL = "jdbc:mysql://localhost/";
+    private static final String DB = "universidadulp_trasversal";
+    private static final String USUARIO = "root";
+    private static String PASSWORD = "";
+
+    private static Connection connection;
+
+    private Conexion() {
+    }
+
+    public static Connection getConexion() {
+        if (connection == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Drivers");
+                connection = DriverManager.getConnection(URL+DB+"?useLegacyDatatimeCode=false&serverTimezone=UTC"
+                        +"&user="+USUARIO+"&password="+PASSWORD);
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Error: error al conectars a la base de datos");
+            } catch (ClassNotFoundException c) {
+                JOptionPane.showMessageDialog(null, "Error: error al cargar los drivers");
+            }
+        }
+        return connection;
+    }
+
 }
